2dd349e4d07e88526995c88e9433b47b2729dc8a
John Maynardk Atienza <142737277+ItsMaynardk@users.noreply.github.com>
2024-10-02 12:45:47 +0800

    Update CardFooter.vue

--- src/components/CardFooter.vue
+++ src/components/CardFooter.vue
@@ -1,27 +1,15 @@
 <template>
     <div>
-        <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto">
-        <div class="text-center my-4 dark:text-white flex items-center justify-center">
-        
-        </div>
-        <div class="text-center">
-            <a href="https://www.linkedin.com/in/johnmaynardkatienza/" target="_blank"
-                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
-                    icon="fa-brands fa-linkedin" /></a>
-            <a href="https://www.facebook.com/maynardkk" target="_blank"
-                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
-                    icon="fa-brands fa-facebook" /></a>
-            <a href="https://www.instagram.com/itsmaynardk/" target="_blank"
-                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"><font-awesome-icon
-                    icon="fa-brands fa-instagram" /></a>
-            <a href="https://github.com/ItsMaynardk" target="_blank"
-                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
-                    icon="fa-brands fa-github" /></a>
-            <a href="mailto:maynardk.atienza@gmail.com"
-                class="m-1 p-1 text-gray-600 text-2xl hover:text-blue-600 dark:text-gray-400"> <font-awesome-icon
-                    icon="fa-solid fa-envelope" /></a>
-               
-        </div>
+      <hr class="border-1 my-8 border-gray-400 sm:w-4/12 mx-auto" />
+      <div class="text-center my-4 dark:text-white flex items-center justify-center">
+        <!-- Empty div or any other footer content you'd like here -->
+      </div>
     </div>
-</template>
-<script setup></script>
+  </template>
+  
+  <script setup></script>
+  
+  <style scoped>
+  /* You can add any additional footer styling here if needed */
+  </style>
+  
